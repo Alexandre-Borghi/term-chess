c77d6bd386ec10ac8caa0d41ee9280533eb1d1a8
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2021-12-26 03:05:27 +0100

    Refactored board to a struct

--- src/main.rs
+++ src/main.rs
@@ -49,55 +49,66 @@ impl Display for Piece {
     }
 }
 
-fn main() {
-    let board = board_new();
-    println!("{}", board_to_str(board));
+struct Board {
+    pieces: [Piece; BOARD_SIZE],
 }
 
-fn board_new() -> [Piece; BOARD_SIZE] {
-    // 00 01 02 03 04 05 06 07
-    // 08 09 10 11 12 13 14 15
-    // 16 17 18 19 20 21 22 23
-    // 24 25 26 27 28 29 30 31
-    // 32 33 34 35 36 37 38 39
-    // 40 41 42 43 44 45 46 47
-    // 48 49 50 51 52 53 54 55
-    // 56 57 58 59 60 61 62 63
+impl Board {
+    fn new() -> Self {
+        // 00 01 02 03 04 05 06 07
+        // 08 09 10 11 12 13 14 15
+        // 16 17 18 19 20 21 22 23
+        // 24 25 26 27 28 29 30 31
+        // 32 33 34 35 36 37 38 39
+        // 40 41 42 43 44 45 46 47
+        // 48 49 50 51 52 53 54 55
+        // 56 57 58 59 60 61 62 63
 
-    let mut board: [Piece; BOARD_SIZE] = [Piece::Blank; BOARD_SIZE];
-    board[0] = Piece::BlackRook;
-    board[1] = Piece::BlackKnight;
-    board[2] = Piece::BlackBishop;
-    board[3] = Piece::BlackQueen;
-    board[4] = Piece::BlackKing;
-    board[5] = Piece::BlackBishop;
-    board[6] = Piece::BlackKnight;
-    board[7] = Piece::BlackRook;
-    for i in 8..=15 {
-        board[i] = Piece::BlackPawn;
-    }
-    for i in 48..=55 {
-        board[i] = Piece::WhitePawn;
-    }
-    board[56 + 0] = Piece::WhiteRook;
-    board[56 + 1] = Piece::WhiteKnight;
-    board[56 + 2] = Piece::WhiteBishop;
-    board[56 + 3] = Piece::WhiteQueen;
-    board[56 + 4] = Piece::WhiteKing;
-    board[56 + 5] = Piece::WhiteBishop;
-    board[56 + 6] = Piece::WhiteKnight;
-    board[56 + 7] = Piece::WhiteRook;
-    board
-}
-
-fn board_to_str(board: [Piece; BOARD_SIZE]) -> String {
-    let mut repr = String::new();
-    for i in 0..BOARD_SIZE {
-        if i != 0 && i % 8 == 0 {
-            repr.push('\n');
+        let mut pieces: [Piece; BOARD_SIZE] = [Piece::Blank; BOARD_SIZE];
+        pieces[0] = Piece::BlackRook;
+        pieces[1] = Piece::BlackKnight;
+        pieces[2] = Piece::BlackBishop;
+        pieces[3] = Piece::BlackQueen;
+        pieces[4] = Piece::BlackKing;
+        pieces[5] = Piece::BlackBishop;
+        pieces[6] = Piece::BlackKnight;
+        pieces[7] = Piece::BlackRook;
+        for i in 8..=15 {
+            pieces[i] = Piece::BlackPawn;
         }
+        for i in 48..=55 {
+            pieces[i] = Piece::WhitePawn;
+        }
+        pieces[56 + 0] = Piece::WhiteRook;
+        pieces[56 + 1] = Piece::WhiteKnight;
+        pieces[56 + 2] = Piece::WhiteBishop;
+        pieces[56 + 3] = Piece::WhiteQueen;
+        pieces[56 + 4] = Piece::WhiteKing;
+        pieces[56 + 5] = Piece::WhiteBishop;
+        pieces[56 + 6] = Piece::WhiteKnight;
+        pieces[56 + 7] = Piece::WhiteRook;
 
-        repr.push_str(format!("{}", board[i]).as_str());
+        Self { pieces }
     }
-    repr
+}
+
+impl Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut repr = String::new();
+        for i in 0..BOARD_SIZE {
+            if i != 0 && i % 8 == 0 {
+                repr.push('\n');
+            }
+
+            repr.push_str(format!("{}", self.pieces[i]).as_str());
+        }
+        f.write_str(repr.as_str())?;
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let board = Board::new();
+    println!("{}", board);
 }
